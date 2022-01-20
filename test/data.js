f3eb127d7dea1e71d3c3f421d0af5bff79f67a8a
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-01-19 17:05:12 -0800

    push with two data.js code

---
+++ test/data.js
@@ -0,0 +1,17 @@
+export const people = [
+    {
+        id: 1,
+        name: 'John',
+        age: 20,
+        bio: 'John  is a senior software engineer at Google.',
+        hobbies: ['hiking', 'cooking', 'coding'],
+    },
+    {
+        id: 2,
+        name: 'Billy',
+        age: 14,
+        bio: 'Billy is a JV basketball play',
+        hobbies: ['basketball', 'video games', 'sleeping'],
+    }
+];
+    
