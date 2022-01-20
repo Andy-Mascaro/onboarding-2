311469c744cf5340a5e8260813a06120fd551ca6
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-01-19 18:00:22 -0800

    exported

--- data.js
+++ data.js
@@ -1,5 +1,30 @@
-export const people = [
+export const people = [ {
     id: 1,
-    name: '' 
+    name: 'Yovana',
+    age: 25, 
+    bio:'Yovana is an artist',
+    hobbies: [ 'paddle boarding','glamping', 'painting'],
+},
+{
+    id: 2,
+    name: 'Andy',
+    age: 29, 
+    bio:'Andy is a gamer who enjoys... ',
+    hobbies: [ 'biking','pool', 'magic'],
+},
+{
+    id: 3,
+    name: 'Bob',
+    age: 40, 
+    bio:'Bob is a musician who loves reggae and music festivals',
+    hobbies: [ 'smoking','dancing', 'vibing'],
+},
+{
+    id: 4,
+    name: 'Mary',
+    age: 50,
+    bio: 'Mary is a senior software engineer at Amazon.',
+    hobbies:['painting','cooking', 'coding'],
+},
+];
 
-]
