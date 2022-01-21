63ce37c3a3d48459b73ac86c4c6e2215a050a865
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-01-20 17:35:51 -0800

    Grab the variables and set the textContent to that user(name, age, bio).

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
 // import functions
 import { people } from './data.js';
 
-console.log(people, 'people');
+// console.log(people, 'people');
 
 // grab DOM elements
-// const nameEl = document.querySelector('.name');
-// const ageE1 = document.querySelector('.age');
-// const bioE1 = document.querySelector('.bio');
-// const hobbiesE1= document.querySelector('.hobbies');
+const selectEl = document.getElementById('name');
+const nameEl = document.querySelector('.name');
+const ageEl = document.querySelector('.age');
+const bioEl = document.querySelector('.bio');
+const hobbiesEl = document.querySelector('.hobbies');
+const container = document.querySelector('.profile');
+// console.log(selectEl);
+
+// console.log('Elements', selectEl, nameEl, ageEl, bioEl, hobbiesEl, container);
+
+function renderPerson(index) {
+    container.classList.add(`${people[index].name}`);
+    nameEl.textContent = 'Name:' + people[index].name;
+    ageEl.textContent = 'age' + people[index].age;
+    bioEl.textContent = 'bio' + people[index].bio;
+
+    for (let hobby of people[index].hobbies) {
+        const li = document.createElement('li');
+        li.textContent = hobby;
+        hobbiesEl.appendChild(li);
+    }
+}
+
 
 // set event listeners 
     // get user input
     // use user input to update state 
     // update DOM to reflect the new state
+selectEl.addEventListener('change', (e) => {
+    console.log('user click');
+});
+
+
