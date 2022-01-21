3c0d69ac6596e6d99ed347d270c3e118abc85162
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-01-20 16:06:59 -0800

    commented out apps.js so try a pull

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ import { people } from './data.js';
 console.log(people, 'people');
 
 // grab DOM elements
-const nameEl = document.querySelector('.name');
-const ageE1 = document.querySelector('.age');
-const bioE1 = document.querySelector('.bio');
-const hobbiesE1= document.querySelector('.hobbies');
+// const nameEl = document.querySelector('.name');
+// const ageE1 = document.querySelector('.age');
+// const bioE1 = document.querySelector('.bio');
+// const hobbiesE1= document.querySelector('.hobbies');
 
 // set event listeners 
     // get user input
